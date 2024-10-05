6c7a2f046189cf3737ba4a21a308f7270808fd68
Valu1212 <toledo.valentina@tecnica7.edu.ar>
2024-10-04 22:13:03 -0300

    commit1

--- Navigation.js
+++ Navigation.js
@@ -1,12 +1,37 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 
 //screens: Pantallas
 
-import HomeScreen from ".screens/HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
 import SettingsScreen from "./screens/SettingsScreen";
 import StackScreen from "./screens/StackScreen";
+
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+
+const HomeStackNavigator = createNativeStackNavigator();
+
+function MyStack() {
+    return (
+        <HomeStackNavigator.Navigator
+            initialRouteName="HomeScreen"
+        >
+            <HomeStackNavigator.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+            /> 
+            <HomeStackNavigator.Screen
+                name="Stack"
+                component={StackScreen}
+            />
+        </HomeStackNavigator.Navigator>   
+    )
+}
+
 
 const Tab = createBottomTabNavigator();
 
@@ -15,13 +40,34 @@ function MyTabs() {
         <Tab.Navigator
             initialRouteName="Home"
             screenOptions= {{
-                tabBarActiveTintColor: "purple",
-
-            
+                tabBarActiveTintColor: "purple", 
             }}
         >
-          <Tab.Screen name="Home" component={HomeScreen}/>
-          <Tab.Screen name="Setting" component={SettingsScreen}/>
+          <Tab.Screen 
+            name="Home" 
+            component={MyStack}
+            options={{
+                tabBarLabel: 'Home',
+                tabBarIcon: ({ color, size}) => (
+                    <MaterialCommunityIcons name= "home" color={color} size={30}/>
+                ),
+                tabBarBadge: 10,
+                headerShown: false,
+            }}
+        />
+
+          <Tab.Screen
+            name="Settings"
+            component={SettingsScreen}
+            options={{
+                tabBarLabel: 'Settings',
+                tabBarIcon: ({ color, size}) => (
+                    <MaterialCommunityIcons name= "brightness-5" color={color} size={30}/>
+                ),
+               
+            }}
+
+        />
         </Tab.Navigator>
     );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,41 @@
-import React from 'react'
-import { View, StyleSheet, TouchableOpacity} from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
+
+  const navigation = useNavigation();
+
   return (
     <View>
-      <Text>
+      <Text
            style={{
             fontSize: 30, 
             textAlign: "center",
             marginTop: "20%"
            }}         
-        HomeScreen</Text>
+        >HomeScreen</Text>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Stack")}
+          style={{
+            backgroundColor: "purple",
+            padding: 10,
+            marginTop: "20%",
+            width: "50%",
+            alignSelf: "center",
+            borderRadius: 10,
+          }}
+        >
+          <Text 
+            style={{
+              fontSize: 15,
+              textAlign: "center",
+              color: "white",
+            }}
+          >Go to Stack Screen</Text>
+        </TouchableOpacity>
     </View>
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
